5c0ecb964ade30330623407643588f4374ed3bb0
dee-rius <darkwahdarius10@gmail.com>
2025-02-27 17:55:27 +0000

    finished implementing confirm button

--- REG-Script.js
+++ REG-Script.js
@@ -1,5 +1,6 @@
 const revealDpInputCheckbox = document.getElementById("add-decimals-checkbox");
 const userDpInput = document.getElementById("decimal-places-input");
+let chosenDecimals = 0;
 
 //so the user decimal place input element is displayed once the user choses to include decimals
 revealDpInputCheckbox.addEventListener('change', displayUserDpInput);
@@ -22,6 +23,9 @@ let maxValue = 0;
 let operators = [];
 const chosenOperators = document.getElementsByName("chosen-operators");
 
+const selectedNumOfNums = document.getElementById("number-of-numbers");
+let numOfNums = 0;
+
 
 const settingsConfirmButton = document.getElementById("settings-confirm-button");
 
@@ -38,14 +42,30 @@ function getSettingValues(){
     console.log("min= " + minValue,"max= " + maxValue);
   }
   
+  //gets user's chosen number of decimals
+  if(revealDpInputCheckbox.checked && userDpInput.value !=""){
+    chosenDecimals = userDpInput.value;
+    console.log("dp value= " + chosenDecimals);
+  }
+  else if(!revealDpInputCheckbox.checked){
+    chosenDecimals = 0;
+    console.log("dp value= " + chosenDecimals);
+  }
+  
   //gets values for chosen operators
     for(chosenOperator of chosenOperators)
    {
-      if(chosenOperator.checked)
-      {
-        operators.splice(operators.length, 0, chosenOperator.value);
-        console.log("Operator value= " + chosenOperator.value)
+    if(chosenOperator.checked)
+    {
+      operators.splice(operators.length, 0, chosenOperator.value);
+      console.log("Operator value= " + chosenOperator.value)
     }
+  }
+  
+  //gets number of numbers to generate
+  if(selectedNumOfNums.value != ""){
+    numOfNums = selectedNumOfNums.value;
+    console.log(numOfNums)
   }
   
 }
