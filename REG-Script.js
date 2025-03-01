3ae13763e7b1b27794157d11804bb56881564e52
dee-rius <darkwahdarius10@gmail.com>
2025-03-01 15:02:37 +0000

    made sure there aren't any loop holes. This includes: the number of numbers input changing float values to integers, swapping max and min values if the latter is greater, as well as, making sure the max and min values have the same decimal places as the usr's chosen the decimals places

--- REG-Script.js
+++ REG-Script.js
@@ -2,6 +2,17 @@ const revealDpInputCheckbox = document.getElementById("add-decimals-checkbox");
 const userDpInput = document.getElementById("decimal-places-input");
 let chosenDecimals = 0;
 
+userDpInput.addEventListener('change', checkValue);
+
+//checks if value of input element exceeds max or is below the min
+function checkValue()
+{
+  if(userDpInput.value > 100 || userDpInput.value < 1)
+  {
+    alert("Number of decimal places should be between 1 and 100");
+  }
+} 
+
 //so the user decimal place input element is displayed once the user choses to include decimals
 revealDpInputCheckbox.addEventListener('change', displayUserDpInput);
 
@@ -20,12 +31,29 @@ let minValue = 0;
 const userMaxValue = document.getElementById("user-max-value");
 let maxValue = 0;
 
+userMaxValue.addEventListener('change', compareValues);
+userMinValue.addEventListener('change', compareValues);
+
+//compares the values of the inputted min and max values
+function compareValues()
+{
+  if(userMinValue.value != "" && userMaxValue.value != "" && userMaxValue.value < userMinValue.value )
+  {
+    alert("The max must be greater or equal to the min, else, the values will be swapped");
+  }
+}
+
 
 const chosenOperators = document.getElementsByName("chosen-operators");
 
 const selectedNumOfNums = document.getElementById("number-of-numbers");
 let numOfNums = 0;
 
+selectedNumOfNums.addEventListener("change", noDecimals)
+
+function noDecimals() {
+  selectedNumOfNums.value = Number(selectedNumOfNums.value).toFixed(0);
+}
 
 const settingsConfirmButton = document.getElementById("settings-confirm-button");
 
@@ -93,6 +121,45 @@ function settingsComplete()
 
 function getSettingValues()
 {
+  //checks if value exceeds tbe max or is below the min
+  if(userDpInput.value > 100)
+  {
+    userDpInput.value = 100;
+  }
+  else if(userDpInput.value < 1)
+  {
+    userDpInput.value = 1;
+  }
+  
+  //gets user's chosen number of decimals
+  if(revealDpInputCheckbox.checked && userDpInput.value !=""){
+    chosenDecimals = Number(userDpInput.value);
+    console.log("dp value= " + chosenDecimals);
+  }
+  else if(!revealDpInputCheckbox.checked){
+    chosenDecimals = 0;
+    console.log("dp value= " + chosenDecimals);
+  }
+  
+  //checks if the user min value is bigger than the user max value and swaps them around
+  if(userMinValue.value > userMaxValue.value)
+  {
+    let swapValue = userMinValue.value;
+    userMinValue.value = userMaxValue.value;
+    userMaxValue.value = swapValue;
+  }
+  
+  //makes sure the chosen min and max values have the same decimal places as the user's decimal places input
+  if(revealDpInputCheckbox.checked && userDpInput.value != "")
+  {
+    userMinValue.value = Number(userMinValue.value).toFixed(chosenDecimals);
+    userMaxValue.value = Number(userMaxValue.value).toFixed(chosenDecimals);
+  }
+  else
+  {
+    userMinValue.value = Number(userMinValue.value).toFixed(0);
+    userMaxValue.value = Number(userMaxValue.value).toFixed(0);
+  }
   
   //gets user's chosen max and min values
     minValue = Number(userMinValue.value);
@@ -100,15 +167,7 @@ function getSettingValues()
     
     console.log("min= " + minValue,"max= " + maxValue);
   
-  //gets user's chosen number of decimals
-  if(revealDpInputCheckbox.checked && userDpInput.value !=""){
-    chosenDecimals = userDpInput.value;
-    console.log("dp value= " + chosenDecimals);
-  }
-  else if(!revealDpInputCheckbox.checked){
-    chosenDecimals = 0;
-    console.log("dp value= " + chosenDecimals);
-  }
+  
   
   //gets number of numbers to generate
     numOfNums = selectedNumOfNums.value;
@@ -143,13 +202,25 @@ function generateEquation()
       equationText.textContent = getRandomInt(maxValue, minValue);
       console.log(equationText.textContent);
       
-      while (numOfNums > 1)
+      while(numOfNums > 1)
       {
         equationText.textContent += operators[getRandomInt(operators.length - 1, 0)] + getRandomInt(maxValue, minValue);
         
         numOfNums -= 1;
       }
     }
+    else if(revealDpInputCheckbox.checked)
+  {
+    equationText.textContent = getRandomFloat(maxValue, minValue).toFixed(chosenDecimals);
+      console.log(equationText.textContent);
+      
+    while(numOfNums > 1)
+    {
+        equationText.textContent += operators[getRandomInt(operators.length - 1, 0)] + getRandomFloat(maxValue, minValue).toFixed(chosenDecimals);
+        
+        numOfNums -= 1;
+    }
+  }
     
     numOfNums = selectedNumOfNums.value;
   }
